0c2920a26846d618d9865a80e2fc2a547ffad44c
Jaifman <jaifman@gmail.com>
2016-09-16 18:29:50 +0200

    Punto de entrada al programa

--- src/PracticaSwing/AplicacionDialogos.java
+++ src/PracticaSwing/AplicacionDialogos.java
@@ -1,9 +1,19 @@
 package PracticaSwing;
 
+import javax.swing.JFrame;
+
 public class AplicacionDialogos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Creamos el Marco
+		Marco miMarco = new Marco();
+		
+		//Establecemos que el programa dejará de ejecutarse al salir				
+		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Hacemos el marco visible
+		miMarco.setVisible(true);
 
 	}
 
