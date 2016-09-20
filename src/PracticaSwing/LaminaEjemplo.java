7aff2c953372aa30c9192d5581a219b8ad91c916
Jaifman <jaifman@gmail.com>
2016-09-20 19:36:05 +0200

    Clase para ejemplificar los objetos Component

---
+++ src/PracticaSwing/LaminaEjemplo.java
@@ -0,0 +1,24 @@
+package PracticaSwing;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JPanel;
+
+public class LaminaEjemplo extends JPanel {
+	
+	public void paintComponent(Graphics g){
+		
+		super.paintComponents(g);
+		
+		Graphics2D g2 = (Graphics2D) g;
+		
+		Rectangle2D rectangulo = new Rectangle2D.Double(0,0,getWidth(),getHeight());
+		
+		g2.setPaint(Color.YELLOW);
+		
+		g2.fill(rectangulo);
+	}
+}
